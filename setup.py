4d157ad776f7bf5c7727c3c6d964853d9ed1ae86
Jose Rodriguez <jmrbcu@gmail.com>
2014-02-28 11:30:24 -0600

    Added support for distribution

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='foundation',
+    version='1.0',
+    packages=['foundation'],
+    url='https://github.com/jmrbcu/foundation',
+    license='GPL2',
+    author='Jose Rodriguez Bacallao',
+    author_email='jmrbcu@gmail.com',
+    description='Extra things to augment the standard library'
+)
