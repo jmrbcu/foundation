701448640bd511c0f7aafc634faf488180d9c090
Jose Rodriguez <jmrbcu@gmail.com>
2014-06-09 15:18:22 -0500

    Added support for command line options

--- foundation/application.py
+++ foundation/application.py
@@ -1,10 +1,11 @@
 # python imports
 import sys
 import logging
+import argparse
 
 # iat imports
 from .conf import SimpleSettings
-from .plugin_manager import PluginManager
+from .plugin_manager import PluginManager, ExtensionPoint
 from .common import get_app_home, get_root_dir
 from .log_utils import setup_root_logger
 from .paths import path
@@ -15,10 +16,17 @@ logger = logging.getLogger(__name__)
 application = None
 
 
+class Argument(object):
+
+    def __init__(self, *args, **kwargs):
+        self.args = args
+        self.kwargs = kwargs
+
+
 class Application(object):
     """The application object"""
 
-    # splashes = ExtensionPoint('imagis.application.splashes')
+    arguments = ExtensionPoint('application.arguments')
 
     def __init__(self, name, version):
         """Initialize an application object with the name and
@@ -30,7 +38,6 @@ class Application(object):
         :param version: application version
         :type version: str
         """
-
         self.about_to_start = Signal()
         self.about_to_quit = Signal()
         self.enabling_plugin = Signal()
@@ -43,6 +50,11 @@ class Application(object):
         # setup application parameters
         self.name = name
         self.version = version
+
+        # setup application command line parser
+        desc = self.name.capitalize() + ' v' + self.version + ' application help'
+        self.option_parser = argparse.ArgumentParser(description=desc)
+        self.options = None
 
         # create application root and home directories
         self.root_dir = path(get_root_dir())
@@ -80,12 +92,18 @@ class Application(object):
             self.user_plugin_dir.makedirs()
 
         dirs = (self.user_plugin_dir, self.plugin_dir)
+        PluginManager.register_extension_point(Application.arguments)
         self.plugin_manager = PluginManager(dirs)
 
     def start(self):
         """Start the application"""
-        logger.info('starting application: %s', self.name)
 
+        # parse the arguments
+        for argument in self.arguments:
+            self.option_parser.add_argument(*argument.args, **argument.kwargs)
+        self.options = self.option_parser.parse_args()
+
+        logger.info('starting application: %s', self.name)
         try:
             # enable all plugins and start the app
             self.plugin_manager.enable_plugins(self._plugin_notification)
