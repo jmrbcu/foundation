8b5ade68cfa56e8e14798640737b737dd06d140d
Jose Rodriguez <jmrbcu@gmail.com>
2014-06-15 23:29:11 -0500

    Brand new tailer implementation from scratch

--- foundation/tailer.py
+++ foundation/tailer.py
@@ -1,250 +1,121 @@
 __author__ = 'jmrbcu'
-import re
 
-gevent_available = False
+# python imports
+import os
+import time
+import logging
+
+# gevent imports
 try:
-    import gevent as time
+    # gevent compatibility
     from gevent.monkey import patch_all
     patch_all(thread=False)
-    gevent_available = True
 except ImportError:
-    import time
+    pass
+
+logger = logging.getLogger(__file__)
 
 
-class Tailer(object):
-    """\
-    Implements tailing and heading functionality like GNU tail and head
-    commands.
-    """
-    line_terminators = ('\r\n', '\n', '\r')
+class Tail(object):
 
-    def __init__(self, file, read_size=1024, end=False):
-        self.read_size = read_size
-        self.file = file
-        self.start_pos = self.file.tell()
-        if end:
-            self.seek_end()
-        self._stop = False
+    HEAD, TAIL = (0, 2)
 
-    def splitlines(self, data):
-        return re.split('|'.join(self.line_terminators), data)
+    def __init__(self, filename, callback, start=HEAD):
+        self.filename = filename
+        self.callback = callback
+        self.fp = None
+        self.start = start
+        self.last_size = 0
+        self.last_inode = -1
+        self.last_line = ''
 
-    def seek_end(self):
-        self.seek(0, 2)
+    def process_file(self):
+        if self.fp is None:
+            try:
+                msg = 'Openning file: {0} for tailing'
+                logger.info(msg.format(self.filename))
+                self.fp = open(self.filename, 'r')
+                self.fp.seek(0, self.start)
+                stat = os.stat(self.filename)
+                self.last_inode = stat.st_ino
+                self.last_size = 0
+            except Exception as e:
+                logger.error('Cannot open the file: {0}'.format(self.filename))
+                if self.fp:
+                    self.fp.close()
+                self.fp = None
+                return
 
-    def seek(self, pos, whence=0):
-        self.file.seek(pos, whence)
+        # check to see if file has moved under us
+        try:
+            stat = os.stat(self.filename)
+            size = stat.st_size
+            inode = stat.st_ino
 
-    def read(self, read_size=None):
-        if read_size:
-            read_str = self.file.read(read_size)
-        else:
-            read_str = self.file.read()
+            if size < self.last_size or inode != self.last_inode:
+                self.last_size = size
+                self.last_inode = inode
+                raise Exception
+        except Exception as e:
+            logger.info('File: {0} was rotated, updating'.format(self.filename))
+            self.fp.close()
+            self.fp = None
+            return
 
-        return len(read_str), read_str
+         # read if size has changed
+        if self.last_size < size:
+            self.last_size = size
+            while True:
+                line = self.fp.readline()
+                time.sleep(0)
+                if not line:
+                    return
 
-    def seek_line_forward(self):
-        """\
-        Searches forward from the current file position for a line terminator
-        and seeks to the charachter after it.
-        """
-        pos = start_pos = self.file.tell()
+                if self.callback:
+                    self.callback(line)
 
-        bytes_read, read_str = self.read(self.read_size)
+    def mainloop(self, sleepfor=0.1):
+        try:
+            while True:
+                self.process_file()
+                time.sleep(sleepfor)
+        except (KeyboardInterrupt, SystemExit):
+            raise
+        finally:
+            if self.fp:
+                self.fp.close()
+                self.fp = None
 
-        start = 0
-        if bytes_read and read_str[0] in self.line_terminators:
-            # The first charachter is a line terminator, don't count this one
-            start += 1
+if __name__ == '__main__':
+    import sys
+    import gevent
 
-        while bytes_read > 0:
-            # Scan forwards, counting the newlines in this bufferfull
-            i = start
-            while i < bytes_read:
-                if read_str[i] in self.line_terminators:
-                    self.seek(pos + i + 1)
-                    return self.file.tell()
-                i += 1
+    logging.basicConfig(level=logging.INFO)
 
-            pos += self.read_size
-            self.seek(pos)
+    def test_writing():
+        with open('test_tailer.txt', 'w') as f:
+            while True:
+                f.write('Line written to the test file from another greenlet\n')
+                f.flush()
+                time.sleep(0.5)
 
-            bytes_read, read_str = self.read(self.read_size)
+    def test_head():
+        try:
+            Tail('test_tailer.txt', sys.stdout.write).mainloop(0.1)
+        except KeyboardInterrupt:
+            pass
 
-        return None
+    def test_tail():
+        try:
+            tailer = Tail('test_tailer.txt', sys.stdout.write, Tail.TAIL)
+            tailer.mainloop(0.1)
+        except KeyboardInterrupt:
+            pass
 
-    def seek_line(self):
-        """\
-        Searches backwards from the current file position for a line terminator
-        and seeks to the charachter after it.
-        """
-        pos = end_pos = self.file.tell()
+    logger.info('STARTING')
+    test_head()
 
-        read_size = self.read_size
-        if pos > read_size:
-            pos -= read_size
-        else:
-            pos = 0
-            read_size = end_pos
 
-        self.seek(pos)
-
-        bytes_read, read_str = self.read(read_size)
-
-        if bytes_read and read_str[-1] in self.line_terminators:
-            # The last charachter is a line terminator, don't count this one
-            bytes_read -= 1
-
-            if read_str[-2:] == '\r\n' and '\r\n' in self.line_terminators:
-                # found crlf
-                bytes_read -= 1
-
-        while bytes_read > 0:
-            # Scan backward, counting the newlines in this bufferfull
-            i = bytes_read - 1
-            while i >= 0:
-                if read_str[i] in self.line_terminators:
-                    self.seek(pos + i + 1)
-                    return self.file.tell()
-                i -= 1
-
-            if pos == 0 or pos - self.read_size < 0:
-                # Not enought lines in the buffer, send the whole file
-                self.seek(0)
-                return None
-
-            pos -= self.read_size
-            self.seek(pos)
-
-            bytes_read, read_str = self.read(self.read_size)
-
-        return None
-
-    def tail(self, lines=10):
-        """\
-        Return the last lines of the file.
-        """
-        self.seek_end()
-        end_pos = self.file.tell()
-
-        for i in xrange(lines):
-            if not self.seek_line():
-                break
-
-        data = self.file.read(end_pos - self.file.tell() - 1)
-        if data:
-            return self.splitlines(data)
-        else:
-            return []
-
-    def head(self, lines=10):
-        """\
-        Return the top lines of the file.
-        """
-        self.seek(0)
-
-        for i in xrange(lines):
-            if not self.seek_line_forward():
-                break
-
-        end_pos = self.file.tell()
-
-        self.seek(0)
-        data = self.file.read(end_pos - 1)
-
-        if data:
-            return self.splitlines(data)
-        else:
-            return []
-
-    def follow(self, delay=1.0):
-        """\
-        Iterator generator that returns lines as data is added to the file.
-
-        Based on: http://aspn.activestate.com/ASPN/Cookbook/Python/Recipe/157035
-        """
-        trailing = True
-        while not self._stop:
-            where = self.file.tell()
-            line = self.file.readline()
-            if line:
-                if trailing and line in self.line_terminators:
-                    # This is just the line terminator added to the end of the file
-                    # before a new line, ignore.
-                    trailing = False
-                    continue
-
-                if line[-1] in self.line_terminators:
-                    line = line[:-1]
-                    if line[-1:] == '\r\n' and '\r\n' in self.line_terminators:
-                        # found crlf
-                        line = line[:-1]
-
-                trailing = False
-                yield line
-                if gevent_available:
-                    time.sleep(0)
-            else:
-                trailing = True
-                self.seek(where)
-                time.sleep(delay)
-
-    def unfollow(self):
-        self._stop = True
-
-    def __iter__(self):
-        return self.follow()
-
-    def close(self):
-        self.file.close()
-
-def tail(file, lines=10):
-    """\
-    Return the last lines of the file.
-
-    >>> import StringIO
-    >>> f = StringIO.StringIO()
-    >>> for i in range(11):
-    ...     f.write('Line %d\\n' % (i + 1))
-    >>> tail(f, 3)
-    ['Line 9', 'Line 10', 'Line 11']
-    """
-    return Tailer(file).tail(lines)
-
-def head(file, lines=10):
-    """\
-    Return the top lines of the file.
-
-    >>> import StringIO
-    >>> f = StringIO.StringIO()
-    >>> for i in range(11):
-    ...     f.write('Line %d\\n' % (i + 1))
-    >>> head(f, 3)
-    ['Line 1', 'Line 2', 'Line 3']
-    """
-    return Tailer(file).head(lines)
-
-def follow(file, delay=1.0):
-    """\
-    Iterator generator that returns lines as data is added to the file.
-
-    >>> import os
-    >>> f = file('test_follow.txt', 'w')
-    >>> fo = file('test_follow.txt', 'r')
-    >>> generator = follow(fo)
-    >>> f.write('Line 1\\n')
-    >>> f.flush()
-    >>> generator.next()
-    'Line 1'
-    >>> f.write('Line 2\\n')
-    >>> f.flush()
-    >>> generator.next()
-    'Line 2'
-    >>> f.close()
-    >>> fo.close()
-    >>> os.remove('test_follow.txt')
-    """
-    return Tailer(file, end=True).follow(delay)
 
 
 
